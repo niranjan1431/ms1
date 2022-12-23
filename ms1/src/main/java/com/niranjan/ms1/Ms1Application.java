dc165004c430d2bed9574e569d6005ea6549ae23
niran <niran@DESKTOP-7J7KI95>
2022-12-23 19:13:40 +0530

    2nd commit

--- ms1/src/main/java/com/niranjan/ms1/Ms1Application.java
+++ ms1/src/main/java/com/niranjan/ms1/Ms1Application.java
@@ -11,7 +11,7 @@ public class Ms1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ms1Application.class, args);
-		System.out.println("project started by me and nandini");
+		System.out.println("project started by me and nandi ni");
 	}
 
 }
